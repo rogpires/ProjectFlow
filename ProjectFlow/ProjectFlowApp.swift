b77d7c3c8ca205a195944cddf2d9be3822fbaf87
Rogerio Cesar Gomes Pires <rogeriocpires@hotmail.com>
2026-05-30 20:30:33 -0300

    Initial Commit

---
+++ ProjectFlow/ProjectFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectFlowApp.swift
+//  ProjectFlow
+//
+//  Created by Rogerio Pires on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
